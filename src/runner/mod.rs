8e276c4ae5e44306a708ae43efb44b19ca149efd
ZENOTME <st810918843@gmail.com>
2024-04-16 23:16:18 +0800

    refine runner

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -17,23 +17,12 @@ pub trait PollerRunner: Clone + Send + Sync + 'static {
 
 /// SingleThreadRunner is a simple runner to run each poller in a single thread. It will bind each poller to a different core in Round Robin way.
 #[derive(Clone)]
-pub struct SingleThreadRunner {
-    core_ids: Vec<CoreId>,
-    next_core_id: Arc<AtomicUsize>,
-}
+pub struct SingleThreadRunner {}
 
 impl SingleThreadRunner {
     /// Create a new SingleThreadRunner.
     pub fn new() -> Self {
-        let mut core_ids = core_affinity::get_core_ids().unwrap();
-        // Make sure the number of cores is even.
-        if core_ids.len() % 2 != 0 {
-            core_ids = core_ids[0..core_ids.len() - 1].to_vec();
-        }
-        Self {
-            core_ids,
-            next_core_id: Arc::new(AtomicUsize::new(10)),
-        }
+        Self {}
     }
 }
 
@@ -44,6 +33,49 @@ impl Default for SingleThreadRunner {
 }
 
 impl PollerRunner for SingleThreadRunner {
+    fn add_poller<T: Poller>(&self, mut poller: T) -> anyhow::Result<JoinHandle<()>> {
+        let handle = thread::spawn(move || {
+            poller.init().unwrap();
+            loop {
+                if let Err(err) = poller.run_once() {
+                    log::error!("Poller run_once failed: {:?}", err);
+                    break;
+                }
+            }
+        });
+        Ok(handle)
+    }
+}
+
+/// Bind to a specific core runner.
+#[derive(Clone)]
+pub struct BindCoreRunner {
+    core_ids: Vec<CoreId>,
+    next_core_id: Arc<AtomicUsize>,
+}
+
+impl BindCoreRunner {
+    /// Create a new SingleThreadRunner.
+    pub fn new() -> Self {
+        let mut core_ids = core_affinity::get_core_ids().unwrap();
+        // Make sure the number of cores is even.
+        if core_ids.len() % 2 != 0 {
+            core_ids = core_ids[0..core_ids.len() - 1].to_vec();
+        }
+        Self {
+            core_ids,
+            next_core_id: Arc::new(AtomicUsize::new(0)),
+        }
+    }
+}
+
+impl Default for BindCoreRunner {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl PollerRunner for BindCoreRunner {
     fn add_poller<T: Poller>(&self, mut poller: T) -> anyhow::Result<JoinHandle<()>> {
         let current = self
             .next_core_id
